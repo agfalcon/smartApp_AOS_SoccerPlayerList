b92a1114c6e5e8e7335e89aa735968213a2720a7
agfalcon <agfalcon12@gmail.com>
2022-12-12 20:52:06 +0900

    connect with server

--- app/src/main/java/kr/ac/kumoh/s20180094/soccerplayerlist/PlayerViewModel.kt
+++ app/src/main/java/kr/ac/kumoh/s20180094/soccerplayerlist/PlayerViewModel.kt
@@ -1,16 +1,22 @@
 package kr.ac.kumoh.s20180094.soccerplayerlist
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.android.volley.Request
 import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import org.json.JSONObject
 
 class PlayerViewModel(application: Application) : AndroidViewModel(application) {
     data class Player(var id: Int, var name: String, var nation: String, var team: String, var image: String)
 
     companion object{
+        const val QUEUE_TAG = "PlayerRequestQueue"
         const val SERVER_URL = "https://expresssongdb-rjhqn.run.goorm.io/"
     }
 
@@ -27,6 +33,37 @@ class PlayerViewModel(application: Application) : AndroidViewModel(application) 
     }
 
     fun requestPlayer(){
-        val request =
+        val request = JsonArrayRequest(
+            Request.Method.GET,
+            "$SERVER_URL/player",
+            null,
+            {
+                list.clear()
+                parseJson(it)
+                _players.value = list
+            },
+            {
+                Toast.makeText(getApplication(), it.toString(), Toast.LENGTH_SHORT).show()
+            }
+        )
+        request.tag = QUEUE_TAG
+        queue.add(request)
+    }
+
+    private fun parseJson(items: JSONArray){
+        for(i in 0 until items.length()){
+            val item = items[i] as JSONObject
+            val id = item.getInt("id")
+            val name = item.getString("name")
+            val nation = item.getString("nation")
+            val team = item.getString("team")
+            val image = item.getString("image")
+            list.add(Player(id,name,nation,team,image))
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        queue.cancelAll(QUEUE_TAG)
     }
 }
