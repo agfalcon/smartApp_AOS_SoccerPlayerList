8d35aaff23017b0c337dfc93be76438860622dbd
agfalcon <agfalcon12@gmail.com>
2022-12-12 21:02:15 +0900

    PlayerAdapter 중간 완료

--- app/src/main/java/kr/ac/kumoh/s20180094/soccerplayerlist/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180094/soccerplayerlist/MainActivity.kt
@@ -2,14 +2,31 @@ package kr.ac.kumoh.s20180094.soccerplayerlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
 import kr.ac.kumoh.s20180094.soccerplayerlist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var model: PlayerViewModel
+    private lateinit var playerAdapter: PlayerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        model = ViewModelProvider(this)[PlayerViewModel::class.java]
+
+        playerAdapter = PlayerAdapter(model)
+
+        binding.listPlayer.apply{
+            layoutManager = LinearLayoutManager(applicationContext)
+            itemAnimator = DefaultItemAnimator()
+            setHasFixedSize(true)
+            adapter = playerAdapter
+        }
     }
 }
---
+++ app/src/main/java/kr/ac/kumoh/s20180094/soccerplayerlist/PlayerAdapter.kt
@@ -0,0 +1,27 @@
+package kr.ac.kumoh.s20180094.soccerplayerlist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.toolbox.NetworkImageView
+
+class PlayerAdapter(private val model: PlayerViewModel): RecyclerView.Adapter<PlayerAdapter.ViewHolder>() {
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+        val playerImage: NetworkImageView = view.findViewById(R.id.img_player)
+        val playerName: TextView = view.findViewById(R.id.text_name)
+        val playerTeam: TextView = view.findViewById(R.id.text_team)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_player, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int = model.players.value?.size ?: 0
+}
